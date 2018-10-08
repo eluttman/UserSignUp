fc6c43e876a66e98338ff903a94df21692b47790
Emily Luttman <emckeever76@gmail.com>
2018-10-07 19:12:58 -0500

    built basics on form

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+#where do i put the autoescape if not using jinja
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/") #add locations
+def display_signup_form():
+    return signup_form.format(#username='', username_error='',
+    #password='', password_error='', email='', email_error='')
+
+@app.route("/")#add locations
+def index():
+    return render_template('index.html')
+
+@app.route("/", methods=['POST']) #add locations, using POST because keeping data
+def hello():
+    username = request.form['username']
+    return render_template('WelcomePage.html',username)
+
+
+
+signup_form = """
+""" #i don't think I still have to do this because we used the html files vs putting html in py file
+
+
+app.run()
