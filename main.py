4dc3e6aa83b6bfc59348002ca4c3506c7ddde423
Emily Luttman <emckeever76@gmail.com>
2018-10-10 20:27:09 -0500

    added username validation

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+
+
 # @app.route("/") #add locations
 # def display_signup_form():
 #     return signup_form.format(#username='', username_error='',
@@ -13,12 +15,28 @@ app.config['DEBUG'] = True
 
 @app.route("/")#add locations
 def index():
-    return render_template('index.html')
+    title = "User signup"
+    return render_template('indexExtendsBase.html', title=title)
 
-# @app.route("/", methods=['POST']) #add locations, using POST because keeping data
-# def hello():
-#     username = request.form['username']
-#     return render_template('WelcomePage.html',username)
+@app.route("/", methods=['POST']) #add locations, using POST because keeping data
+def hello():
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify']
+    email = request.form['email']
+
+    username_errors = False
+
+    if len(username) < 3 or len(username) > 20:
+        username_errors = True
+    elif " " in username:
+        username_errors = True
+
+    
+    if username_errors:
+        return render_template('indexExtendsBase.html', title="User signup", username_errors=username_errors, username=username)
+    else:
+        return render_template('WelcomePage.html')
 
 # signup_form = """
 # """ #i don't think I still have to do this because we used the html files vs putting html in py file
